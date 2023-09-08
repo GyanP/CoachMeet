84b947aaa3f2f1a139e2c84c06274d6974237460
GyanP <gyanig72@gmail.com>
2023-09-08 18:49:05 +0530

    stream video conferencing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,8 @@ function App() {
 	const [ me, setMe ] = useState("")
 	const [ stream, setStream ] = useState()
 	const [ receivingCall, setReceivingCall ] = useState(false)
+	const [ caller, setCaller ] = useState("")
+	const [ callerSignal, setCallerSignal ] = useState()
 	const [ callAccepted, setCallAccepted ] = useState(false)
 	const [ idToCall, setIdToCall ] = useState("")
 	const [ name, setName ] = useState("")
@@ -34,7 +36,9 @@ function App() {
 
 		socket.on("callUser", (data) => {
 			setReceivingCall(true)
+			setCaller(data.from)
 			setName(data.name)
+			setCallerSignal(data.signal)
 		})
 	}, [])
 
@@ -65,6 +69,24 @@ function App() {
 		connectionRef.current = peer
 	}
 
+	const answerCall =() =>  {
+		setCallAccepted(true)
+		const peer = new Peer({
+			initiator: false,
+			trickle: false,
+			stream: stream
+		})
+		peer.on("signal", (data) => {
+			socket.emit("answerCall", { signal: data, to: caller })
+		})
+		peer.on("stream", (stream) => {
+			userVideo.current.srcObject = stream
+		})
+
+		peer.signal(callerSignal)
+		connectionRef.current = peer
+	}
+
 	return (
 		<>
 			<h1 style={{ textAlign: "center", color: '#fff' }}>Video Conferencing</h1>
@@ -74,7 +96,7 @@ function App() {
 					{stream &&  <video playsInline muted ref={myVideo} autoPlay style={{ width: "300px" }} />}
 				</div>
 				<div className="video">
-					{callAccepted ?
+					{callAccepted  ?
 					<video playsInline ref={userVideo} autoPlay style={{ width: "300px"}} />:
 					null}
 				</div>
@@ -102,7 +124,7 @@ function App() {
 					onChange={(e) => setIdToCall(e.target.value)}
 				/>
 				<div className="call-button">
-					{callAccepted ? (
+					{callAccepted  ? (
 						<Button variant="contained" color="secondary">
 							End Call
 						</Button>
@@ -118,7 +140,7 @@ function App() {
 				{receivingCall && !callAccepted ? (
 						<div className="caller">
 						<h1 >{name} is calling...</h1>
-						<Button variant="contained" color="primary">
+						<Button variant="contained" color="primary" onClick={answerCall}>
 							Answer
 						</Button>
 					</div>
--- server.js
+++ server.js
@@ -10,7 +10,19 @@ const io = require("socket.io")(server, {
 })
 
 io.on("connection", (socket) => {
-	console.log(socket)
+	socket.emit("me", socket.id)
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	})
+
+	socket.on("callUser", (data) => {
+		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+	})
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	})
 })
 
 server.listen(5000, () => console.log("server is running on port 5000"))
