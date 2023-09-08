ede0a7e3f3764df793f2d719ccf07e6137e35895
GyanP <gyanig72@gmail.com>
2023-09-08 18:27:15 +0530

    socket connectivity FE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
-import React from "react"
+import React, {useEffect} from "react"
+import io from "socket.io-client"
 import "./App.css"
 
+
+const socket = io.connect('http://localhost:5000')
+
 function App() {
+
+	useEffect(() => {
+		socket.emit("connection", "frontend connected")
+	}, [])
 
 	return (
 		<>
